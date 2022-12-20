89f4bdbafd632f4e9b4f0f1e1e74f8706feb516e
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-12-20 14:21:06 +0700

    HB01 presentation layer for books

--- app/src/main/java/com/alexeyyuditsky/holybibleapp/core/BibleApp.kt
+++ app/src/main/java/com/alexeyyuditsky/holybibleapp/core/BibleApp.kt
@@ -1,12 +1,28 @@
 package com.alexeyyuditsky.holybibleapp.core
 
 import android.app.Application
+import com.alexeyyuditsky.holybibleapp.domain.BooksInteractor
+import com.alexeyyuditsky.holybibleapp.presentation.BaseBooksDomainToUiMapper
+import com.alexeyyuditsky.holybibleapp.presentation.BooksCommunication
+import com.alexeyyuditsky.holybibleapp.presentation.MainViewModel
+import com.alexeyyuditsky.holybibleapp.presentation.ResourceProvider
 
 class BibleApp : Application() {
 
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate() {
         super.onCreate()
-        
+
+        val bookInteractor: BooksInteractor = TODO()
+        mainViewModel = MainViewModel(
+            bookInteractor,
+            BaseBooksDomainToUiMapper(
+                BooksCommunication.Base(),
+                ResourceProvider.Base(this),
+            ),
+            BooksCommunication.Base()
+        )
     }
 
 }
--- app/src/main/java/com/alexeyyuditsky/holybibleapp/presentation/BookUi.kt
+++
@@ -1,7 +0,0 @@
-package com.alexeyyuditsky.holybibleapp.presentation
-
-import com.alexeyyuditsky.holybibleapp.core.Abstract
-
-sealed class BookUi : Abstract.Object<Unit, Abstract.Mapper.Empty>() {
-    // todo
-}
